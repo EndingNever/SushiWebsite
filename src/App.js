100005f577b3c1ff531e43f564f50de670aa9379
Fabian Villasenor <exionite@gmail.com>
2022-03-16 11:34:45 -0700

    layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
 import Body from './components/Body/Body';
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
@@ -9,7 +10,12 @@ function App() {
       <div className='header-component'>
         <Header />
       </div>
-
+      <div className="body-component">
+        <Body />
+      </div>
+      <div className="portfolio-component">
+        <Portfolio />
+      </div>
     </div>
   );
 }
